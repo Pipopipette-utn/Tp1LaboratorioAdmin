5b214745a8dc88ae761ea1bcaa7d804c4a19dcc6
jazminRillo <jazmin.rillo@gmail.com>
2024-03-27 13:43:04 -0300

    Cambiando color jeje

--- src/components/commons/AppMenu.tsx
+++ src/components/commons/AppMenu.tsx
@@ -1,6 +1,4 @@
-import * as React from "react";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
@@ -9,7 +7,20 @@ export const AppMenu = () => {
 	return (
 		<AppBar position="sticky">
 			<Toolbar>
-				<Typography variant="h6" component="a" sx={{ flexGrow: 1 }} href="/">
+				<Typography
+					variant="h6"
+					component="a"
+					sx={{
+						flexGrow: 1,
+						color: "white",
+						"&:hover": {
+							color: "white",
+
+							cursor: "pointer",
+						},
+					}}
+					href="/"
+				>
 					Portal de noticias
 				</Typography>
 				<Button color="inherit" href="/empresas">
